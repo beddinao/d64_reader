8f5124b2621dfeaa297cfa5c959d43d6b9dae17c
beddinao <bilaleddinaoui@gmail.com>
2025-01-13 13:18:00 +0100

    initiation

---
+++ include/d64_reader.h
@@ -0,0 +1,8 @@
+#ifndef D64_READER
+#define D64_READER
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#endif
---
+++ src/d64_reader.c
@@ -0,0 +1,9 @@
+#include "d64_reader.h"
+
+
+int	main(int c, char **v) {
+	if (c != 2) {
+		printf("usage: %s <d64 file>\n", v[0]);
+		return 1;
+	}
+}
